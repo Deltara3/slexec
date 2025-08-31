35e4e33440a8de7b1b074b049525ac2cd2e319b0
Deltara <50772027+Deltara3@users.noreply.github.com>
2025-08-31 16:49:23 -0400

    tweak: nevermind we don't need fancy types where we're going

--- src/raw.rs
+++ src/raw.rs
@@ -1,34 +1,20 @@
-use std::ffi::c_void;
-use std::os::raw::{c_char, c_int};
-
-/// Pointer to a raw C string.
-type StringPtr = *const c_char;
-
-/// Pointer to a dynamic-link module.
-type ModulePtr = *mut c_void;
-
-/// Pointer to a symbol in a dynamic-link module.
-type SymbolPtr = *mut c_void;
-
-/// Result of an operation.
-type OperationResult = c_int;
-
 #[cfg(windows)]
 mod win32 {
-    use super::{StringPtr, ModulePtr, SymbolPtr, OperationResult};
+    use std::ffi::c_void;
+    use std::os::raw::{c_char, c_int};
 
     #[link(name = "kernel32")]
     unsafe extern "system" {
         /// Loads the specified module into the address space of the calling process.
         #[link_name = "LoadLibraryA"]
-        fn load_library(library_filename: StringPtr) -> ModulePtr;
+        fn load_library(library_filename: *const c_char) -> *mut c_void;
 
         /// Retrieves the address of an exported symbol from a dynamic-link library.
         #[link_name = "GetProcAddress"]
-        fn get_proc_address(library_module: ModulePtr, symbol_name: StringPtr) -> SymbolPtr;
+        fn get_proc_address(library_module: *mut c_void, symbol_name: *const c_char) -> *mut c_void;
 
         /// Frees a loaded dynamic-link library module.
         #[link_name = "FreeLibrary"]
-        fn free_library(library_module: ModulePtr) -> OperationResult;
+        fn free_library(library_module: *mut c_void) -> *mut c_int;
     }
 }
